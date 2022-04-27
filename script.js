a449403f9a3d3cbade42d5985fa9f83d1afc78c4
Piorek1994 <pprus94@gmail.com>
2022-04-27 19:28:44 +0200

    Project finished

--- script.js
+++ script.js
@@ -1,15 +1,32 @@
 const counterItems = document.querySelectorAll('.counter')
 const counterBox = document.querySelector('.counter-box')
 
-
 const options = {
-    rootMargin: '-250px'
+	rootMargin: '-250px',
 }
 
 const startCounter = entry => {
-    
-}
+	if (entry[0].isIntersecting) {
+		counterItems.forEach(counter => {
 
+
+			const updateCounter = () => {
+				const finalNumber = counter.getAttribute('data-number')
+                const value = parseInt(counter.textContent)
+
+                const speed = finalNumber / 8
+
+                if(value < finalNumber){
+                    counter.textContent = `${Math.floor(value + speed)}`
+                    setTimeout(updateCounter, 80)
+                } else {
+                    counter.textContent = finalNumber
+                }
+			}
+            updateCounter()
+		})
+	}
+}
 
 const observer = new IntersectionObserver(startCounter, options)
 observer.observe(counterBox)
